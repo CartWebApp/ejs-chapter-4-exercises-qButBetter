8a0e01f1ed642823654fd2d5e109d6cf12a383c7
James Steed <james.steed23@student.cart.org>
2024-01-30 08:34:53 -0800

    finished Arrays[0] notes

---
+++ Steed-Arrays-0.js
@@ -0,0 +1,18 @@
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn Kubernetes'];
+
+const hobbies = ['Video Games', 'Coding', 'Reading'];
+console.log(hobbies);
+
+let listItem = hobbies[0];
+console.log(listItem);
+console.log(hobbies[2]);
+console.log(hobbies[3]);
+
+let seasons = ['Winter', 'Spring', 'Summer', 'Fall'];
+seasons[3] = 'Autumn';
+console.log(seasons);
+
+newYearsResolutions[0] = 'Bench 110 lbs';
+newYearsResolutions[1] = 'Pass all classes with an A';
+newYearsResolutions[2] = 'Get a 3 on AP Calculus AB test';
+console.log(newYearsResolutions);
