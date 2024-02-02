0e8598076faba5ced4e52550ab53c12008d61ef0
James Steed <james.steed23@student.cart.org>
2024-02-02 10:29:08 -0800

    built more of deepEqual

--- list.js
+++ list.js
@@ -1,6 +1,4 @@
 function arrayToList(arrayInput) {
-  // arrayInput[value]
-
   let listOutput = null;
 
   for (i = arrayInput.length - 1; i >= 0; i--) {
@@ -11,8 +9,6 @@ function arrayToList(arrayInput) {
 }
 
 function listToArray(listInput) {
-  // listInput[value]
-
   // Might have to define length of the array
   let arrayOutput = [];
   const objList = listInput;
@@ -32,7 +28,6 @@ function prepend(element, listInput) {
 
 function nth(listInput, num) {
   let arr = listToArray(listInput);
-  let counter = 0;
 
   for (i = 0; i < arr.length; i++) {
     if (i == num) {
@@ -55,13 +50,10 @@ let list = {
 
 // tests
 console.log(arrayToList([10, 20]));
-// // → {value: 10, rest: {value: 20, rest: null}}
-// console.log(listToArray(arrayToList([10, 20, 30])));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
 // // → [10, 20, 30]
-// console.log(prepend(10, prepend(20, null)));
+console.log(prepend(10, prepend(20, null)));
 // // → {value: 10, rest: {value: 20, rest: null}}
-
-
-// Fix this one
 console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
